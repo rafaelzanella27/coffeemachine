ddb849a4d1c709979ea727360a934d942e7505de
razrodrigues <rafael.rodrigues@klab.com.br>
2022-01-27 15:19:24 -0300

    Ajustes no service

--- src/main/java/com/estudo/cmachine/service/MaquinaBasicaService.java
+++ src/main/java/com/estudo/cmachine/service/MaquinaBasicaService.java
@@ -1,29 +1,20 @@
 package com.estudo.cmachine.service;
 
-import com.estudo.cmachine.builders.Builder;
 import com.estudo.cmachine.builders.MaquinaDeCafeBasicaBuilder;
 import com.estudo.cmachine.components.BebidaDeCafe;
 import com.estudo.cmachine.director.Director;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MaquinaBasicaService {
 
-    @Autowired(required = false)
-    private Builder builder;
-
-    @Autowired(required = false)
-    private Director director;
-
-    @Autowired(required = false)
-    private MaquinaDeCafeBasicaBuilder maquinaDeCafeBasicaBuilder;
 
     public BebidaDeCafe execute(){
         Director director = new Director();
         MaquinaDeCafeBasicaBuilder maquinaDeCafeBasicaBuilder = new MaquinaDeCafeBasicaBuilder();
 
         director.construirMaquinaDeCafeBasica(maquinaDeCafeBasicaBuilder);
+
         return maquinaDeCafeBasicaBuilder.fazCafeFiltrado();
     }
 }
